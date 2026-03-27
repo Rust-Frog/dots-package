a5993c64b23115ddc6b3338f3ba5e9093322b8a6
Rust-Frog <b235.dev@gmail.com>
2026-03-28 01:14:21 +0800

    Fix: use curl instead of wget in font download
    The Docker container doesn't have wget installed. Switched to curl which is available.

--- build.sh
+++ build.sh
@@ -170,7 +170,7 @@ mkdir -p "$FONT_INSTALL/.config/fontconfig"
 
 # Download Noto Color Emoji
 cd /tmp
-wget -q --show-progress "https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf" -O NotoColorEmoji.ttf
+curl -L --show-progress -o NotoColorEmoji.ttf "https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf"
 cp NotoColorEmoji.ttf "$FONT_INSTALL/.local/share/fonts/"
 
 # Create fontconfig to prefer emoji font
